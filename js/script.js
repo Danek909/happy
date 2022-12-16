b58602bf6aa88139264894d2d8731ff3b8f7f64d
Danya <kogdana@gmail.com>
2022-12-16 16:16:47 +0200

    gf

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.divSlick').slick({
+        infinite: true,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+        speed: 1000,
+    });
+});
